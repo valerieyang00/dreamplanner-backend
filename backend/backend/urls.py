7d3155d6003432328e654da85b7da7b4bb9ada6c
valerieyang00 <valerieyang00@gmail.com>
2022-10-25 20:06:35 -0700

    models set up complete

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from dreamplanner import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserView, 'dreamplanner')
+router.register(r'destinations', views.DestinationView, 'dreamplanner')
+router.register(r'expenses', views.ExpenseView, 'dreamplanner')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
--- backend/dreamplanner/models.py
+++ backend/dreamplanner/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+
+class Destination(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='destinations')
+    name = models.CharField(max_length=100)
+    budget = models.FloatField()
+    photo = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Expense(models.Model):
+    date = models.DateField(auto_now=False)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='expenses')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='expenses')
+    class Category(models.TextChoices):
+        Transportation = 'transportation'
+        Lodging = 'lodging'
+        Food = 'food'
+        Activities = 'activities'
+        Miscellaneous = 'misc'
+    category = models.CharField(max_length=50, choices=Category.choices)
+    merchant = models.CharField(max_length=100)
+    amount = models.FloatField()
+    details = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.amount
 
 # Create your models here.
