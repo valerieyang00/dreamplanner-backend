17ce1ec0f2e6d0f60b7b66790e829d849b01aab1
valerieyang00 <valerieyang00@gmail.com>
2022-10-30 16:34:35 -0700

    back to working status

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,10 +146,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [ 'http://localhost:3000' ]
 
-import dj_database_url
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
