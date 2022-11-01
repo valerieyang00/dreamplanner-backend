05285ca7703e335d0e6914b0c62271c9da9a61b1
valerieyang00 <valerieyang00@gmail.com>
2022-11-01 10:39:50 -0700

    fix settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://dreamplanner-backend-production.up.railway.app/']
+ALLOWED_HOSTS = ['dreamplanner-backend-production.up.railway.app']
 
 
 # Application definition
